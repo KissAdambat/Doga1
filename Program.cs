28b3658f39a763a12dd65dbc334c97a818dd6f9c
KissAdam <kissa@kkszki.hu>
2024-10-18 10:23:32 +0200

    Ready to use

--- Program.cs
+++ Program.cs
@@ -12,18 +12,38 @@ namespace Doga1
         static void ertekeles()
         {
             Console.WriteLine("Kérem adja meg a pont számot");
-            pontszam = Convert.ToInt32(Console.ReadLine());
+            int    pontszam = Convert.ToInt32(Console.ReadLine());
 
             if (pontszam < 0 || pontszam > 100)
             {
                 Console.WriteLine("Érvénytelen pontszám.");
+            }
+            else if (pontszam > 0 && pontszam < 50)
+            {
+                Console.WriteLine("Elégtelen");
+            }
+            else if (pontszam > 49 && pontszam < 60)
+            {
+                Console.WriteLine("Elégséges");
+            }
+            else if (pontszam > 59 && pontszam < 80)
+            {
+                Console.WriteLine("Közepes");
+            }
+            else if (pontszam > 79 && pontszam < 90)
+            {
+                Console.WriteLine("Jó");
+            }
+            else if (pontszam > 89 && pontszam < 101)
+            {
+                Console.WriteLine("Jeles");
             }
         }
 
 
         static void Main(string[] args)
         {
-
+            ertekeles();
         }
     }
 }
