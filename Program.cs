b70dd97e7ad2ac697d708118405e6862fee1b3d4
KissAdam <kissa@kkszki.hu>
2024-10-18 10:18:43 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Doga1
+{
+    internal class Program
+    {
+        static void ertekeles()
+        {
+            Console.WriteLine("Kérem adja meg a pont számot");
+            pontszam = Convert.ToInt32(Console.ReadLine());
+
+            if (pontszam < 0 || pontszam > 100)
+            {
+                Console.WriteLine("Érvénytelen pontszám.");
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
